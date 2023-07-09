5de3bff62358e795da8782052d1242c72624bd05
4ndr33w <rx.081@hotmail.com>
2023-07-09 21:21:51 +0500

    Добавьте файлы проекта.

---
+++ 9_07_2023_Planner/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using _9_07_2023_Planner.ViewModels;
+using _9_07_2023_Planner.Views.Windows;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _9_07_2023_Planner
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        MainWindowViewModel _vm;
+
+        public App() 
+        {
+            new MainWindow() { DataContext = _vm }.ShowDialog();
+        }
+    }
+}
---
+++ 9_07_2023_Planner/Infrastructure/RelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _9_07_2023_Planner.Infrastructure
+{
+    public class RelayCommand : ICommand
+    {
+        readonly Action<object> _execute;
+        readonly Func<object, bool> _canExecute;
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            if (_canExecute != null) return _canExecute(parameter);
+            return true;
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            if (_execute != null) _execute(parameter);
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        public RelayCommand(Action<object> execute)
+        {
+            _execute = execute;
+        }
+    }
+}
