44f39579db7a237cd52930746d33d925ad80a5a8
4ndr33w <rx.081@hotmail.com>
2023-07-10 21:02:00 +0500

    Продолжаю оформлять интерфейс главного окна

--- 9_07_2023_Planner/Views/Windows/MainWindow.xaml.cs
+++ 9_07_2023_Planner/Views/Windows/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,8 +20,38 @@ namespace _9_07_2023_Planner.Views.Windows
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            //if (PropertyChanged != null) 
+            //{ 
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            //}
+        }
+        //private double _windowHeight;
+        //public double WindowHeight
+        //{
+        //    get { return _windowHeight; }
+        //    set
+        //    {
+        //        //_windowHeight = value; 
+        //        _windowHeight = this.Height;
+        //        OnPropertyChanged(nameof(WindowHeight));
+        //    }
+        //}
+
+        //private double _panelHeight;
+        //public double PanelHeight
+        //{
+        //    get { return _panelHeight; }
+        //    set
+        //    {
+        //        _panelHeight = WindowHeight - 50; OnPropertyChanged(nameof(PanelHeight));
+        //    }
+        //}
         public MainWindow()
         {
             InitializeComponent();
